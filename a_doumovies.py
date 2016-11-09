71baaaee81acb9ef0eb2080a841cfba450fd9c44
zhangjie <zhangjie@reyun.com>
2016-11-09 16:38:13 +0800

    change get way for movie's info

--- a_doumovies.py
+++ a_doumovies.py
@@ -93,28 +93,45 @@ def get_detail(detail_url):
         for line in info.split("\n"):
             if line.find(":") > 0:
                 list_info.append(line.strip())
-        print(list_info)
         dict_info = {}
         for item in list_info:
-            dict_info[item.split(":")[0]] = item.split(":")[1]
-        for key in dict_info:
-            print(key)
-            print(dict_info[key])
+            dict_info[item.split(":")[0]] = item.split(":")[1].strip()
 
-        if len(list_info) == 10:
-            item_movie.director, item_movie.screenwriter, item_movie.performer, item_movie.genre, \
-            item_movie.country, item_movie.language, item_movie.release_time, item_movie.length, \
-            item_movie.another_name, item_movie.imdb = list_info
+        director = dict_info.get("导演")
+        screenwriter = dict_info.get("编剧")
+        performer = dict_info.get("主演")
+
+        genre = dict_info.get("类型")
+        country = dict_info.get("制片国家/地区")
+        language = dict_info.get("语言")
+
+        release_time = dict_info.get("上映日期")
+        length = dict_info.get("片长")
+        another_name = dict_info.get("又名")
+        imdb = dict_info.get("IMDb链接")
 
         item_movie.url = url
         item_movie.img_url = mainpic.find("a").find("img").get("src")
         item_movie.name, item_movie.year = [item.get_text() for item in content.find("h1").find_all("span")]
 
+        item_movie.director = director if director else ""
+        item_movie.screenwriter = screenwriter if screenwriter else ""
+        item_movie.performer = performer if performer else ""
+
+        item_movie.genre = genre if genre else ""
+        item_movie.country = country if country else ""
+        item_movie.language = language if language else ""
+
+        item_movie.release_time = release_time if release_time else ""
+        item_movie.length = length if length else ""
+        item_movie.another_name = another_name if another_name else ""
+        item_movie.imdb = imdb if imdb else ""
+
         # print(item_movie.director, item_movie.screenwriter, item_movie.performer, item_movie.genre,
         #       item_movie.country, item_movie.language, item_movie.release_time, item_movie.length,
         #       item_movie.another_name, item_movie.imdb, item_movie.url, item_movie.img_url,
         #       item_movie.name, item_movie.year)
-        # save_movies(item_movie)
+        save_movies(item_movie)
     except error.HTTPError as ex:
         logging.error("Url_fetcher error: %s", ex)
     return
