d967f41da408731ce3d958db9772f6e5d7e5b8dd
jessie <liuliruoxue@163.com>
2016-11-09 16:19:17 +0800

    update to get info and add something in .gitignore

--- a_doumovies.py
+++ a_doumovies.py
@@ -1,10 +1,14 @@
 # _*_ coding: utf-8 _*_
 
+import re
+import json
 import logging
+import sqlalchemy
 from queue import Queue
 from bs4 import BeautifulSoup
+from sqlalchemy.orm import sessionmaker
 from urllib import request, parse, error
-from z_classmovie import *
+from z_alche import *
 
 queue_url = Queue()
 
@@ -23,11 +27,13 @@ def get_urls():
 
         for i in range(len(list_tags)):
             for item in list_tags[i]:
-                url_classify = parse.urljoin(base="https://movie.douban.com/tag/%E7%88%B1%E6%83%85", url=item.a["href"])
+                url_classify = parse.urljoin(base="https://movie.douban.com/tag/", url=item.a["href"])
                 classify_comment = item.get_text().replace(")", "").split("(")
                 item_classify = list_title[i] + ":" + classify_comment[0]
                 comment_count = classify_comment[1]
                 list_url_tags.append([item_classify, url_classify, comment_count, "base"])
+                while True:
+                    url_more = url_classify + "?start=" + str(i) + "type=T"
                 queue_url.put([item_classify, url_classify, comment_count, "base"])
     except error.HTTPError as ex:
         logging.error("Get_urls error: %s", ex)
@@ -54,22 +60,15 @@ def url_fetcher(classify_item):
         except error.HTTPError as ex:
             logging.error("Url_fetcher error: %s", ex)
     elif flag == "detail":
-        try:
-            item_movie = Movie
-            url = parse.quote("https://movie.douban.com/subject/26280528/", safe="%/:=&?~#+!$,;'@()*[]|")
-            header = {"Accept-encoding": "utf-8"}
-            req = request.Request(url=url, headers=header)
-            resp = request.urlopen(req)
-            soup = BeautifulSoup(resp, "html5lib")
-
-        except error.HTTPError as ex:
-            logging.error("Url_fetcher error: %s", ex)
+        get_detail(url)
+    else:
+        logging.error("Url: %s, Unknown flag :%s", url, flag)
 
 
-def get_detail():
+def get_detail(detail_url):
     try:
-        item_movie = Movie
-        url = parse.quote("https://movie.douban.com/subject/26280528/", safe="%/:=&?~#+!$,;'@()*[]|")
+        item_movie = Movie()
+        url = parse.quote(detail_url, safe="%/:=&?~#+!$,;'@()*[]|")
         header = {"Accept-encoding": "utf-8"}
         req = request.Request(url=url, headers=header)
         resp = request.urlopen(req)
@@ -77,12 +76,30 @@ def get_detail():
         content = soup.find("div", id="content")
         article = soup.find("div", class_="article")
         mainpic = article.find("div", id="mainpic")
+        dou_score = article.find("div", class_="rating_wrap clearbox")
+        dou_beterthan = article.find("div", class_="rating_betterthan")
         info = article.find("div", id="info").get_text()
         list_info = []
 
+        list_score = [item.get_text() for item in dou_score.find_all("span")[1::2]]
+        str_starpercent = re.sub(pattern="[\[\]'\s]", repl="", string=str(list_score))
+
+        item_movie.score = dou_score.find("strong").get_text()
+        list_starpercent = str_starpercent.split(",")
+        item_movie.comment, *star_percent = list_starpercent
+        item_movie.star_percent = re.sub(pattern="[\[\]']", repl="", string=str(star_percent))
+        item_movie.better_than = re.sub(pattern="[\s]", repl="", string=(dou_beterthan.get_text().strip().replace("\n", ",")))
+
         for line in info.split("\n"):
             if line.find(":") > 0:
-                list_info.append(line.strip().split(":")[1])
+                list_info.append(line.strip())
+        print(list_info)
+        dict_info = {}
+        for item in list_info:
+            dict_info[item.split(":")[0]] = item.split(":")[1]
+        for key in dict_info:
+            print(key)
+            print(dict_info[key])
 
         if len(list_info) == 10:
             item_movie.director, item_movie.screenwriter, item_movie.performer, item_movie.genre, \
@@ -93,15 +110,27 @@ def get_detail():
         item_movie.img_url = mainpic.find("a").find("img").get("src")
         item_movie.name, item_movie.year = [item.get_text() for item in content.find("h1").find_all("span")]
 
-        print(item_movie.director, item_movie.screenwriter, item_movie.performer, item_movie.genre,
-              item_movie.country, item_movie.language, item_movie.release_time, item_movie.length,
-              item_movie.another_name, item_movie.imdb, item_movie.url, item_movie.img_url, item_movie.name, item_movie.year)
-
+        # print(item_movie.director, item_movie.screenwriter, item_movie.performer, item_movie.genre,
+        #       item_movie.country, item_movie.language, item_movie.release_time, item_movie.length,
+        #       item_movie.another_name, item_movie.imdb, item_movie.url, item_movie.img_url,
+        #       item_movie.name, item_movie.year)
+        # save_movies(item_movie)
     except error.HTTPError as ex:
         logging.error("Url_fetcher error: %s", ex)
+    return
+
+
+def save_movies(movie):
+    engine = sqlalchemy.create_engine("mysql+pymysql://root:123@localhost:3306/db_doumovies?charset=utf8", echo=True)
+    print("2222")
+    db_session = sessionmaker(bind=engine)
+    session = db_session()
+    session.add(movie)
+    session.commit()
+    return
 
 
 if __name__ == "__main__":
     # get_urls()
     # url_fetcher(queue_url.get())
-    get_detail()
+    get_detail("https://movie.douban.com/subject/26340522/")
--- z_alche.py
+++ z_alche.py
@@ -2,24 +2,52 @@
 
 import sqlalchemy
 from sqlalchemy.ext import declarative
+# from sqlalchemy.orm import sessionmaker
 
-engine = sqlalchemy.create_engine("mysql+pymysql://root:123@localhost:3306/movie_db", encoding="utf8", echo=True)
-connection = engine.connect()
-
-connection = connection.execution_options(
-    # 默认为 REPEATABLE READ connection.get_isolation_level()可查看
-    isolation_level="READ COMMITTED"
-)
+# engine = sqlalchemy.create_engine("mysql+pymysql://root:123@localhost:3306/db_doumovies?charset=utf8", echo=True)
 
 BaseModel = declarative.declarative_base()
-
-t = sqlalchemy.Table("dou_movie", metadata,)
 
 
 # 电影模型
 class Movie(BaseModel):
-    __tablename__ = "dou_movie"
+    __tablename__ = "tb_doumovies"
     __table_args__ = {
         "mysql_engine": "InnoDB",
         "mysql_charset": "utf8"
     }
+
+    id = sqlalchemy.Column("m_id", sqlalchemy.INT, primary_key=True, autoincrement=True)
+    url = sqlalchemy.Column("m_url", sqlalchemy.VARCHAR)
+    img_url = sqlalchemy.Column("m_imgurl", sqlalchemy.VARCHAR)
+    name = sqlalchemy.Column("m_name", sqlalchemy.VARCHAR)
+    year = sqlalchemy.Column("m_year", sqlalchemy.VARCHAR)
+
+    director = sqlalchemy.Column("m_director", sqlalchemy.VARCHAR)
+    screenwriter = sqlalchemy.Column("m_screenwriter", sqlalchemy.VARCHAR)
+    performer = sqlalchemy.Column("m_performer", sqlalchemy.VARCHAR)
+
+    genre = sqlalchemy.Column("m_genre", sqlalchemy.VARCHAR)
+    country = sqlalchemy.Column("m_country", sqlalchemy.VARCHAR)
+    language = sqlalchemy.Column("m_language", sqlalchemy.VARCHAR)
+
+    release_time = sqlalchemy.Column("m_releasetime", sqlalchemy.VARCHAR)
+    length = sqlalchemy.Column("m_length", sqlalchemy.VARCHAR)
+    another_name = sqlalchemy.Column("m_anothername", sqlalchemy.VARCHAR)
+
+    score = sqlalchemy.Column("m_score", sqlalchemy.Float)
+    comment = sqlalchemy.Column("m_comment", sqlalchemy.VARCHAR)
+    star_percent = sqlalchemy.Column("m_starpercent", sqlalchemy.VARCHAR)
+    better_than = sqlalchemy.Column("m_betterthan", sqlalchemy.VARCHAR)
+    imdb = sqlalchemy.Column("m_imdb", sqlalchemy.VARCHAR)
+
+
+# DBSession = sessionmaker(bind=engine)
+# session = DBSession()
+#
+# session.add(Movie(url="www.baidu.com", img_url="www.baidu.com", name="模型", year="2016",
+#                   director="导演", screen_writer="编辑", performer="演员",
+#                   genre="类别", country="国家", language="语言",
+#                   release_time="上映时间", length="时长", another_name="别名",
+#                   score=10.0, comment="评价", star_percent="星级比例", imdb="imdb"))
+# session.commit()
