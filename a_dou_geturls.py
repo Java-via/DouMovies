f776961d05b7e9621244fafeb595572a45f8f396
zhangjie <zhangjie@reyun.com>
2016-11-10 16:53:13 +0800

    add header to handle 403

--- a_dou_geturls.py
+++ a_dou_geturls.py
@@ -1,6 +1,7 @@
 # _*_ coding: utf-8 _*_
 
 import logging
+from queue import Queue
 from bs4 import BeautifulSoup
 from urllib import request, parse, error
 
@@ -11,21 +12,35 @@ def get_urls(queue_url):
 
     url = "https://movie.douban.com/tag/"
     try:
-        cookie = "_vwo_uuid_v2=E3DF1C1851C8DCC6D914A081334D0A07|113f38aee5ecb34b708c3639c0c6d4b1;_" \
-                 "pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1478687617%2C%22https%3A%2F%2F" \
-                 "www.baidu.com%2Fs%3Fwd%3D%25E8%25B1%2586%25E7%2593%25A3%26rsv_spt%3D1%26" \
-                 "rsv_iqid%3D0x8cda56f40002035a%26issp%3D1%26f%3D8%26rsv_bp%3D0%26rsv_id" \
-                 "x%3D2%26ie%3Dutf-8%26tn%3D99055797_hao_pg%26rsv_enter%3D1%26rsv_sug3%3D5%26" \
-                 "rsv_sug1%3D5%26rsv_sug7%3D101%22%5D"
-        headers = {"Accept-encoding": "utf-8",
-                   "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                                 "Chrome/41.0.2272.89 Safari/537.36",
-                   "cookie": cookie}
+        accept_encoding = "utf-8"
+        accept_language = "zh-CN,zh;q=0.8"
+        cookie = """ll="108288"; bid=KRgXJ-HZyqg; ap=1; ct=y; _
+        pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1478765905%2C%22
+        https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3DFxfrUj65Jds79lwLnCNI-7fZRzFXv2YS_m2kdKN4Qm0d
+        jOP_HzOoHfQ1PcHYu6tt%26wd%3D%26eqid%3D82773cc90001f0b50000000258242468%22%5D; _
+        pk_id.100001.4cf6=c4c0fd1104728cb0.1477665951.13.1478765905.1478763637.;
+        _pk_ses.100001.4cf6=*; __utma=30149280.408279850.1477665952.1478691365.1478763631.10;
+        __utmb=30149280.4.7.1478764168948; __utmc=30149280;
+        __utmz=30149280.1478763631.10.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic;
+        __utma=223695111.1703187677.1477665952.1478763631.1478765905.10; __utmb=223695111.0.10.1478765905;
+        __utmc=223695111; __utmz=223695111.1478763631.9.6.utmcsr=baidu|utmccn=(organic)|utmcmd=organic;
+        _vwo_uuid_v2=E3DF1C1851C8DCC6D914A081334D0A07|113f38aee5ecb34b708c3639c0c6d4b1"""
+        host = "movie.douban.com"
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) " \
+                     "AppleWebKit/537.36 (KHTML, like Gecko) " \
+                     "Chrome/53.0.2785.116 Safari/537.36"
+
+        headers = {"Accept-encoding": accept_encoding,
+                   "Accept-language": accept_language,
+                   "User-Agent": user_agent,
+                   "Cookie": cookie,
+                   "Host": host}
         req = request.Request(url=url, data=None, headers=headers)
         resp = request.urlopen(req)
         soup = BeautifulSoup(resp, "html5lib")
+        print(soup)
         tb = soup.find("div", class_="clearfix")
-
+        print(tb)
         list_title = [item.get_text().replace("·", "").strip() for item in tb.find_all("a", class_="tag-title-wrapper")]
         list_tags = [item.find_all("td") for item in tb.find_all("tbody")]
 
@@ -42,5 +57,6 @@ def get_urls(queue_url):
     print(queue_url.qsize())
     return queue_url
 
-# if __name__ == '__main__':
-#     get_urls(queue_url)
+if __name__ == '__main__':
+    queue_url = Queue()
+    get_urls(queue_url)
--- b_dou_fetcher.py
+++ b_dou_fetcher.py
@@ -11,16 +11,36 @@ logging.basicConfig(level=logging.DEBUG)
 
 def url_fetcher(queue_url, queue_save):
     while queue_url.qsize() > 0:
-        time.sleep(2)
+        time.sleep(3)
         print(queue_url.qsize())
         classify, url, comment_count, flag = queue_url.get()
         if flag == "base":
             try:
                 url = parse.quote(url, safe="%/:=&?~#+!$,;'@()*[]|")
-                header = {"Accept-encoding": "utf-8",
-                          'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                                        'Chrome/41.0.2272.89 Safari/537.36'}
-                req = request.Request(url=url, headers=header)
+                accept_encoding = "utf-8"
+                accept_language = "zh-CN,zh;q=0.8"
+                cookie = """ll="108288"; bid=KRgXJ-HZyqg; ap=1; ct=y; _
+                    pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1478765905%2C%22
+                    https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3DFxfrUj65Jds79lwLnCNI-7fZRzFXv2YS_m2kdKN4Qm0d
+                    jOP_HzOoHfQ1PcHYu6tt%26wd%3D%26eqid%3D82773cc90001f0b50000000258242468%22%5D; _
+                    pk_id.100001.4cf6=c4c0fd1104728cb0.1477665951.13.1478765905.1478763637.;
+                    _pk_ses.100001.4cf6=*; __utma=30149280.408279850.1477665952.1478691365.1478763631.10;
+                    __utmb=30149280.4.7.1478764168948; __utmc=30149280;
+                    __utmz=30149280.1478763631.10.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic;
+                    __utma=223695111.1703187677.1477665952.1478763631.1478765905.10; __utmb=223695111.0.10.1478765905;
+                    __utmc=223695111; __utmz=223695111.1478763631.9.6.utmcsr=baidu|utmccn=(organic)|utmcmd=organic;
+                    _vwo_uuid_v2=E3DF1C1851C8DCC6D914A081334D0A07|113f38aee5ecb34b708c3639c0c6d4b1"""
+                host = "movie.douban.com"
+                user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) " \
+                             "AppleWebKit/537.36 (KHTML, like Gecko) " \
+                             "Chrome/53.0.2785.116 Safari/537.36"
+
+                headers = {"Accept-encoding": accept_encoding,
+                           "Accept-language": accept_language,
+                           "User-Agent": user_agent,
+                           "Cookie": cookie,
+                           "Host": host}
+                req = request.Request(url=url, data=None, headers=headers)
                 resp = request.urlopen(req)
                 soup = BeautifulSoup(resp, "html5lib")
 
